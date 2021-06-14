84f311e44095b3be0b79c3de93295b49866ba4f5
l3nnartt <loesche.lennart@gmail.com>
2021-06-14 19:57:22 +0200

    Add Name Placeholder

--- src/main/java/com/github/l3nnartt/MessageReceiveListener.java
+++ src/main/java/com/github/l3nnartt/MessageReceiveListener.java
@@ -2,19 +2,25 @@ package com.github.l3nnartt;
 
 import net.labymod.api.events.MessageReceiveEvent;
 import net.labymod.main.LabyMod;
+import net.labymod.utils.ModColor;
 import net.minecraft.client.Minecraft;
 
 public class MessageReceiveListener implements MessageReceiveEvent {
     @Override
     public boolean onReceive(String clean, String formatted) {
+
+        System.out.println(clean);
+        System.out.println(formatted);
+
         if (LabyMod.getInstance().getPlayerName().equals("TimoliaBar")) {
             if (Barkeeper.getInstance().isEnabledBarkeeper()) {
 
                 if (clean.toLowerCase().contains("@TimoliaBar".toLowerCase())) {
+
                     boolean found = false;
                     for (DataContainer data : Barkeeper.getInstance().getConfigHandler().getQestions()) {
                         if (containsIgnoreCase(clean, data.getContains())) {
-                            Minecraft.getMinecraft().thePlayer.sendChatMessage(data.getAnswer());
+                            Minecraft.getMinecraft().thePlayer.sendChatMessage(data.getAnswer().replace("%user%", getNameFromMessage(clean)));
                             found = true;
                             break;
                         }
@@ -32,6 +38,10 @@ public class MessageReceiveListener implements MessageReceiveEvent {
 
     public boolean containsIgnoreCase(String string, String contains){
         return string.toLowerCase().contains(contains.toLowerCase());
+    }
+
+    public String getNameFromMessage(String msg){
+        return ModColor.removeColor(msg.split("§r§7:")[0]);
     }
 
 }
